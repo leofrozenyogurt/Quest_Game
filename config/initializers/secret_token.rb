d639e5539401296fef90d3fd8e7419e3e1534ae2
siameseguy <psyhakhom@gmail.com>
2012-09-25 20:26:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdventureGame::Application.config.secret_token = '65afefba8c9061b560d376d69821f353d89b55295089d8768c1c394bb1ea66a74a3af98ac9b901acb2c3734d64d0fda0133ebfaa747f5764993c535bba05d3cf'
